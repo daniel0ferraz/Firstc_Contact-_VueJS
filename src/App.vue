76ccc8a6f2e845ca7276a89b66ff01c0d9b7902d
Daniel Ferraz <Daniel0ferraz@outlook.com>
2022-08-15 14:26:47 -0300

    Criando Primeiro componente

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <OlaMundo />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
+import OlaMundo from './components/OlaMundo.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
-  }
+    // HelloWorld,
+    OlaMundo,
+  },
 });
 </script>
 
---
+++ src/components/OlaMundo.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Olá mundo!</h1>
+  <h2>Primeiro componente com VUE 3 ;(</h2>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'OlaMundo',
+});
+</script>
+
+<style>
+h1 {
+  color: green;
+}
+</style>
